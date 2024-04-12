d1687c74be45480d9d92829b23b2bd3db0ccc9ce
Sara Arvizu <sara.arvizu07@gmail.com>
2024-04-12 11:06:29 -0600

    Estilos y funciones get, put, delete

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,246 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState, useEffect } from "react";
 import Axios from "axios";
 
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css";
+import Swal from 'sweetalert2'
 
+
+
+function App() {
+  // Obtener datos
+  const [id, setId] = useState();
   const [nombre, setNombre] = useState("");
   const [telefono, setTelefono] = useState("");
   const [correo, setCorreo] = useState("");
   const [fechaDeNacimiento, setFechaDeNacimiento] = useState("");
   const [sexo, setSexo] = useState("");
 
-  const add = ()=>{
-    Axios.post("http://localhost:3001/create",{
-      nombre:nombre,
-      telefono:telefono,
-      correo:correo,
-      fechaDeNacimiento:fechaDeNacimiento,
-      sexo:sexo,
+  // Botones de editar y eliminar
+  const [editar, setEditar] = useState(false);
+
+  // Crear una lista
+  const [usuariosList, setUsuarios] = useState([]);
+
+  useEffect(() => {
+    // Llama a la función getUsuarios() solo una vez cuando el componente se monta
+    getUsuarios();
+  }, []); // El segundo parámetro [] indica que este efecto se ejecuta solo una vez al montar el componente
+
+  //Crear
+  const add = () => {
+    Axios.post("http://localhost:3001/create", {
+      nombre: nombre,
+      telefono: telefono,
+      correo: correo,
+      fechaDeNacimiento: fechaDeNacimiento,
+      sexo: sexo,
     }).then(() => {
-      alert("Usuario registrado");
-    }).catch(error => {
-      console.error("Error al registrar usuario:", error);
-      alert("Error al registrar usuario");
+      getUsuarios(); // Llama a getUsuarios después de agregar un nuevo usuario
+      limpiarCampos()
     });
-  }
+  };
+
+
+//Editar usuarios
+  const update = () => {
+    Axios.put("http://localhost:3001/update", {
+      id: id,
+      nombre: nombre,
+      telefono: telefono,
+      correo: correo,
+      fechaDeNacimiento: fechaDeNacimiento,
+      sexo: sexo,
+    }).then(() => {
+      getUsuarios(); // Llama a getUsuarios después de agregar un nuevo usuario
+      limpiarCampos()
+    });
+  };
+
+  //Eliminar usuarios
+ //Eliminar usuarios
+const deleteUsuarios = (id) => {
+  Axios.delete(`http://localhost:3001/delete/${id}`).then(() => {
+    getUsuarios(); // Llama a getUsuarios después de eliminar un usuario
+    limpiarCampos();
+  }).catch((error) => {
+    console.error("Error al eliminar usuario:", error);
+    // Aquí podrías manejar el error de alguna manera, por ejemplo, mostrando una alerta
+    Swal.fire({
+      icon: 'error',
+      title: 'Error al eliminar usuario',
+      text: 'Hubo un problema al intentar eliminar el usuario. Por favor, inténtalo de nuevo más tarde.',
+    });
+  });
+};
+
+  const limpiarCampos = ()=>{
+    setNombre("");
+    setTelefono("");
+    setCorreo("");
+    setFechaDeNacimiento("");
+    setSexo("");
+    setId("");
+    setEditar(false);
+  };
+
+
+  const editarUsuario = (val) => {
+    setEditar(true);
+
+    setId(val.id);
+    setNombre(val.nombre);
+    setTelefono(val.telefono);
+    setCorreo(val.correo);
+    setFechaDeNacimiento(val.fechaDeNacimiento);
+    setSexo(val.sexo);
+  };
+
+ //Lista de usuarios
+ const getUsuarios = () => {
+  Axios.get("http://localhost:3001/usuarios").then((response) => {
+    setUsuarios(response.data);
+  });
+};  
 
 
   return (
-    <div className="App">
-     <div className="datos">
-     <label>Nombre: <input onChange={(event) => setNombre(event.target.value)} type="text" /></label>
-        <label>Teléfono: <input onChange={(event) => setTelefono(event.target.value)} type="text" /></label>
-        <label>Correo: <input onChange={(event) => setCorreo(event.target.value)} type="email" /></label>
-        <label>Fecha de nacimiento: <input onChange={(event) => setFechaDeNacimiento(event.target.value)} type="date" /></label>
-        <label>Sexo: <input onChange={(event) => setSexo(event.target.value)} type="text" /></label>
+    <div className="container">
+      <div className="card text-center">
+        <div className="card-header">GESTIÓN DE USUARIOS</div>
+        <div className="card-body">
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">
+              Nombre:
+            </span>
+            <input
+              type="text"
+              onChange={(event) => setNombre(event.target.value)}
+              className="form-control"
+              value={nombre}
+              placeholder="Ingrese nombre"
+              aria-label="Nombre"
+              aria-describedby="basic-addon1"
+            />
+          </div>
 
-        <button onClick={add}>Registrar</button>
-      <button>Editar</button>
-     </div>
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">
+              Teléfono:
+            </span>
+            <input
+              type="text"
+              className="form-control"
+              onChange={(event) => setTelefono(event.target.value)}
+              value={telefono}
+              placeholder="Ingrese teléfono"
+              aria-label="Teléfono"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">
+              Correo:
+            </span>
+            <input
+              type="email"
+              className="form-control"
+              onChange={(event) => setCorreo(event.target.value)}
+              value={correo}
+              placeholder="Ingrese correo"
+              aria-label="Correo"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">
+              Fecha de nacimiento:
+            </span>
+            <input
+              type="date"
+              className="form-control"
+              onChange={(event) => setFechaDeNacimiento(event.target.value)}
+              value={fechaDeNacimiento || ""}
+              aria-label="Fecha de nacimiento"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">
+              Sexo:
+            </span>
+            <input
+              type="text"
+              className="form-control"
+              onChange={(event) => setSexo(event.target.value)}
+              placeholder="Ingrese sexo"
+              value={sexo}
+              aria-label="Sexo"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+
+          <div className="card-footer text-muted">
+            {editar ? 
+              <div>
+              <button className="btn btn-warning m-2" onClick={update}>
+                Actualizar
+              </button> 
+              <button className="btn btn-info m-2" onClick={limpiarCampos}>
+                Cancelar
+              </button>
+              </div>
+             : 
+              <button className="btn btn-success" onClick={add}>
+                Registrar
+              </button>
+            }
+          </div>
+        </div>
+        <table className="table table-success table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Nombre</th>
+              <th scope="col">Correo</th>
+              <th scope="col">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* Map sobre la lista de usuarios para crear filas de tabla */}
+            {usuariosList.map((val, key) => (
+              <tr key={val.id}>
+                <th>{val.id}</th>
+                <td>{val.nombre}</td>
+                <td>{val.correo}</td>
+                <td>
+                  <div
+                    className="btn-group"
+                    role="group"
+                    aria-label="Basic mixed styles example"
+                  >
+                    <button
+                      type="button"
+                      onClick={() => editarUsuario(val)}
+                      className="btn btn-info"
+                    >
+                      Editar
+                    </button>
+                    <button type="button"
+                    onClick={() => deleteUsuarios(val.id)}
+                    className="btn btn-danger">
+                      Eliminar
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const db = mysql.createConnection({
     password:"",
     database:"usuarios"
 });
-
+// POST Crear nuevos usuarios
 app.post("/create",(req,res)=>{
     const nombre = req.body.nombre;
     const telefono = req.body.telefono;
@@ -26,13 +26,72 @@ app.post("/create",(req,res)=>{
         if(err){
             console.log(err)
         } else{
-            res.send("Usuario registrado con éxito!!");
+            res.send(result);
         }
     }
     );
  
 });
 
+// GET devuelve lista de usuarios exclusivamente con el id, nombre y correo.
+app.get("/usuarios",(req,res)=>{
+
+    db.query("SELECT id, nombre, correo FROM usuarios",
+
+    (err,result)=>{
+        if(err){
+            console.log(err)
+        } else{
+            res.send(result);
+        }
+    }
+    );
+ 
+});
+
+// PUT para editar datos del usuario
+
+app.put("/update",(req,res)=>{
+    const id = req.body.id;
+    const nombre = req.body.nombre;
+    const telefono = req.body.telefono;
+    const correo = req.body.correo;
+    const fechaDeNacimiento = req.body.fechaDeNacimiento; 
+    const sexo = req.body.sexo;
+
+    db.query("UPDATE usuarios SET nombre=?, telefono=?, correo=?, fechaDeNacimiento=?, sexo=? WHERE id=? ",
+  [nombre, telefono, correo, fechaDeNacimiento, sexo, id],
+
+
+    (err,result)=>{
+        if(err){
+            console.log(err)
+        } else{
+            res.send("Usuario actualizado con éxito");
+        }
+    }
+    );
+ 
+});
+
+
+// Eliminar usuarios Delete
+app.delete("/delete/:id",(req,res)=>{ 
+    const id = req.params.id; 
+
+    db.query("DELETE FROM usuarios WHERE id=? ",[id], 
+
+
+    (err,result)=>{
+        if(err){
+            console.log(err)
+        } else{
+            res.send("Usuario ELIMINADO con éxito");
+        }
+    }
+    );
+ 
+}); 
 app.listen(3001,()=>{
     console.log("Corriendo en el puerto 3001")
 })
